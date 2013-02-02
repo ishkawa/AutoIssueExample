1a55e76e48a19e42e5e4f62b2e7d58f6382b0a97
Yosuke Ishikawa <ishkawa73@gmail.com>
2013-02-03 07:22:01 +0900

    clean

--- AutoIssueExample/AIEAppDelegate.m
+++ AutoIssueExample/AIEAppDelegate.m
@@ -4,22 +4,10 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-    // TODO: hogehoge
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     self.window.backgroundColor = [UIColor whiteColor];
     self.window.rootViewController = [[UIViewController alloc] init];
     [self.window makeKeyAndVisible];
-    
-    // TODO: fugafuga
-    self.window.rootViewController = [[UIViewController alloc] init];
-    
-    // FIXME: piyopiyo
-    self.window.rootViewController = [[UIViewController alloc] init];
-    
-    // TODO: zzzzzzzzzzzzzzzzzzzzzzz
-    for (NSInteger i=0; i<10; i++) {
-        NSLog(@"hoge: %@", window);
-    }
     
     return YES;
 }
