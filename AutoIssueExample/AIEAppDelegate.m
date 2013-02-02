687c5878544b4434cd9ebfda5a3bbfe132d47e2c
Yosuke Ishikawa <ishkawa73@gmail.com>
2013-02-02 20:57:57 +0900

    hogehoge

--- AutoIssueExample/AIEAppDelegate.m
+++ AutoIssueExample/AIEAppDelegate.m
@@ -4,9 +4,12 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+    // TODO: hogehoge
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     self.window.backgroundColor = [UIColor whiteColor];
+    self.window.rootViewController = [[UIViewController alloc] init];
     [self.window makeKeyAndVisible];
+    
     return YES;
 }
 
